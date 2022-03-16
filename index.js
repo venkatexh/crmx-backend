1b7d4bb54b756da02ca094259615be01b2563e99
Venkatesh Chakrabarty <venkatesh@chaosnative.com>
2022-03-16 16:52:19 +0530

    Update port
    Signed-off-by: Venkatesh Chakrabarty <venkatesh@chaosnative.com>

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(function (req, res, next) {
 
 app.use(errorHandler);
 
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}..`);
